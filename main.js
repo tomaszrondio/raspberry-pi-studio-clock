12f3f63fcf0a1cf6121b8c8980f1e1832a14b9e6
Tomasz Rondio <tomasz.rondio@gmail.com>
2018-03-26 00:42:12 +0200

    init

---
+++ main.js
@@ -0,0 +1,48 @@
+
+$(document).ready(function(){
+  for(var i = 0;i<60;i++){
+    $('.container1').append('<div class="seconds" />');
+  }
+
+  var deg = 264;
+  for(var i = 1;i<61;i++){
+    deg=deg+6;
+    $('.seconds:nth-child('+i+')').css({
+      '-webkit-transform' : 'rotate('+deg+'deg) translatex(45vh)',
+      'opacity' : '0.1'
+    });
+  }
+
+  ////////////////////////////////////////////////////////////////////////////
+
+  var t = setInterval(function(){
+    var d = new Date();
+    var h = d.getHours();
+    var m = d.getMinutes();
+    var s = d.getSeconds();
+
+    for(var i = 1;i<=s+1;i++){
+      if(s===0){
+        $('.seconds').css({
+          'opacity' : '0.1'
+        });
+      }
+      $('.seconds:nth-child('+i+')').css({
+        'opacity' : '1'
+      });
+    }
+
+    if(h<10){
+      h = '0' + h;
+    }
+
+    if(m<10){
+      m = '0' + m;
+    }
+    if(s<10){
+      s = '0' + s;
+    }
+    var time = h + ":" + m + ":" + s;
+    $('.time').text(time);
+  },1000);
+});
